88e8aad9f038c2a10af658015ada638b0f61e772
Milos Kotlar <kotlarmilos@gmail.com>
2022-02-16 15:59:24 +0100

    update

---
+++ client/assets-client.js
@@ -0,0 +1,101 @@
+const AbstractClient = require("./abstract-client");
+const AssetsProxyPath = require("../utilities/assets-proxy-path");
+
+class AssetsClient extends AbstractClient {
+    constructor(options) {
+        super(options);
+        this._assetsProxyPath = new AssetsProxyPath(options);
+    }
+
+    /**
+     * @param content
+     * @param {object} options
+     * @param {string} options.filepath - path to the dataset
+     * @param {string[]} options.keywords (optional)
+     */
+    create(content, options) {
+        options.content = content;
+        options.method = 'provision';
+        return new Promise((resolve, reject) => {
+            this._publishRequest(options)
+                .then((response) =>
+                    this._getResult({
+                        handler_id: response.data.handler_id,
+                        operation: options.method,
+                    })
+                )
+                .then((response) => {
+                    resolve(response);
+                })
+                .catch((error) => reject(error));
+        });
+    }
+
+    /**
+     * @param {object} options
+     * @param {string} options.filepath - path to the dataset
+     * @param {string[]} options.keywords (optional)
+     */
+    update(content, ual, options) {
+        options.content = content;
+        options.ual = ual;
+        options.method = 'update';
+        return new Promise((resolve, reject) => {
+            this._publishRequest(options)
+                .then((response) =>
+                    this._getResult({
+                        handler_id: response.data.handler_id,
+                        operation: options.method,
+                    })
+                )
+                .then((response) => {
+                    resolve(response);
+                })
+                .catch((error) => reject(error));
+        });
+    }
+
+    async get(ual, commitHash) {
+        //TODO add cache
+
+        let result;
+        if (commitHash)
+            result = await this.resolve({ids: [commitHash]});
+        else
+            result = await this.resolve({ids: [ual]});
+        if (result.status === this.STATUSES.completed) {
+            const data = result.data[0].result;
+
+            return this._assetsProxyPath.createPath(
+                Object.assign(Object.create(null), undefined, undefined),
+                Object.assign(Object.create(null), undefined, data),
+                ual
+            );
+        }
+
+        return undefined;
+    }
+
+    async getStateCommitHashes(ual) {
+        //TODO add cache
+
+        let result = await this.resolve({ids: [ual]});
+        if (result.status === this.STATUSES.completed) {
+            return result.data[0].result.assertions;
+        }
+        return undefined;
+    }
+
+
+    transfer(options) {
+        //TODO
+    }
+
+    approve(options) {
+        //TODO
+    }
+
+
+}
+
+module.exports = AssetsClient;
